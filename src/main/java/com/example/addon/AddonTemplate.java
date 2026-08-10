be17e2f7a0f84547da5a18cc00e8048a21f459fe
ayantemerkhanov-arch <ayantemerkhanov@gmail.com>
2026-08-10 05:15:23 +0500

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,36 +1,16 @@
 package com.example.addon;
 
 import com.example.addon.modules.TargetStrafe;
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
-import com.mojang.logging.LogUtils;
-import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import org.slf4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Category CATEGORY = new Category("HvH");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
-
-        // Modules
         Modules.get().add(new TargetStrafe());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
     }
 
     @Override
@@ -41,10 +21,5 @@ public class AddonTemplate extends MeteorAddon {
     @Override
     public String getPackage() {
         return "com.example.addon";
-    }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
     }
 }
